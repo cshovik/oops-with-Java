bfa2b59df2ffd0056e10a305244e633df6faa2e8
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2023-07-30 20:04:14 +0530

    Create input and output.java

---
+++ Variables and Data Types/input and output.java
@@ -0,0 +1,30 @@
+import java.util.*;//for giving input called this package
+
+public class JavaBasics {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        String input= sc.nextLine();
+        System.out.println(input);
+
+        int number= sc.nextInt();
+        System.out.println(number);
+
+        float price = sc.nextFloat();
+        System.out.println(price);
+
+    }
+}
+
+/*
+next
+nextLine
+nextByte
+nextInt
+nextChar
+nextShort
+nextByte
+nextLong
+nextDouble
+nextBoolean
+*/
